bfef5f9ac911caf5e9ea4d18ebc8121ec5cafed6
mavis <h07h07h347@gmail.com>
2026-08-20 02:16:04 +0300

    refactor(audio): optimize delay effect with asymmetric prime buffers

--- audio/effects_delay.go
+++ audio/effects_delay.go
@@ -3,26 +3,30 @@ package audio
 import "github.com/gopxl/beep/v2"
 
 const (
-	delayBufferSize = 8820
-	tailSamplesMax  = 44100
-	delayDryGain    = 0.85
-	delayWetGain    = 0.4
+	delayBufferSizeL = 8389
+	delayBufferSizeR = 9221
+	delayFeedback    = 0.45
+	tailSamplesMax   = 31000
+	delayDryGain     = 0.85
+	delayWetGain     = 0.3
 )
 
 type delayStreamer struct {
 	streamer    beep.Streamer
 	lBuffer     []float64
 	rBuffer     []float64
-	counter     int
+	lCounter    int
+	rCounter    int
 	tailSamples int
 }
 
 func applyDelay(streamer beep.Streamer) beep.Streamer {
 	return &delayStreamer{
 		streamer:    streamer,
-		lBuffer:     make([]float64, delayBufferSize),
-		rBuffer:     make([]float64, delayBufferSize),
-		counter:     0,
+		lBuffer:     make([]float64, delayBufferSizeL),
+		rBuffer:     make([]float64, delayBufferSizeR),
+		lCounter:    0,
+		rCounter:    0,
 		tailSamples: tailSamplesMax,
 	}
 }
@@ -43,26 +47,35 @@ func (d *delayStreamer) Stream(samples [][2]float64) (n int, ok bool) {
 		limit = len(samples)
 	}
 	i := 0
+
 	for ; i < limit; i++ {
+		dryL := 0.0
+		dryR := 0.0
 		if i < n {
-			echoL := d.lBuffer[d.counter]
-			echoR := d.rBuffer[d.counter]
-			samples[i][0] = samples[i][0]*delayDryGain + echoL*delayWetGain
-			d.rBuffer[d.counter] = samples[i][0]
-			samples[i][1] = samples[i][1]*delayDryGain + echoR*delayWetGain
-			d.lBuffer[d.counter] = samples[i][1]
+			dryL = samples[i][0]
+			dryR = samples[i][1]
 		} else {
-			echoL := d.lBuffer[d.counter]
-			echoR := d.rBuffer[d.counter]
-			samples[i][0] = echoL * delayWetGain
-			d.rBuffer[d.counter] = samples[i][0]
-			samples[i][1] = echoR * delayWetGain
-			d.lBuffer[d.counter] = samples[i][1]
 			d.tailSamples--
 		}
-		d.counter++
-		if d.counter >= len(d.lBuffer) {
-			d.counter = 0
+		echoL := d.lBuffer[d.lCounter]
+		echoR := d.rBuffer[d.rCounter]
+
+		outL := dryL*delayDryGain + echoL*delayWetGain
+		outR := dryR*delayDryGain + echoR*delayWetGain
+
+		samples[i][0] = outL
+		samples[i][1] = outR
+
+		d.rBuffer[d.rCounter] = dryL + (echoL * delayFeedback)
+		d.lBuffer[d.lCounter] = dryR + (echoR * delayFeedback)
+
+		d.lCounter++
+		if d.lCounter >= len(d.lBuffer) {
+			d.lCounter = 0
+		}
+		d.rCounter++
+		if d.rCounter >= len(d.rBuffer) {
+			d.rCounter = 0
 		}
 		if d.tailSamples == 0 {
 			i++
@@ -70,7 +83,6 @@ func (d *delayStreamer) Stream(samples [][2]float64) (n int, ok bool) {
 			break
 		}
 	}
-
 	return i, ok
 }
 
