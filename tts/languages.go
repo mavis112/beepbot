4bdd8187d66ea5756c0c0d08310c53dfef9ca677
mavis <h07h07h347@gmail.com>
2026-09-01 00:04:48 +0300

    refactor(bot): add funnebot to beepbot message converter, add and change some languages in ttsLanguages map

--- tts/languages.go
+++ tts/languages.go
@@ -4,33 +4,45 @@ func NewTtsLanguages() map[string]string {
 	return map[string]string{
 		"ru": "ru-RU",
 		"ua": "uk-UA",
+		"uk": "uk-UA",
 		"et": "et-EE",
 		"lt": "lt-LT",
 		"lv": "lv-LV",
 
 		"en": "en-US",
+		"us": "en-US",
 		"gb": "en-GB",
 		"au": "en-AU",
 
 		"jp": "ja-JP",
 		"cn": "zh-CN",
+		"kr": "ko-KR",
+		"vi": "vi-VN",
 
-		"pl":  "pl-PL",
-		"deu": "de-DE",
-		"ita": "it-IT",
-		"fr":  "fr-FR",
-		"esp": "es-ES",
-		"pt":  "pt-PT",
-		"ro":  "ro-RO",
-		"tur": "tr-TR",
+		"pl": "pl-PL",
+		"sk": "sk-SK",
+		"sr": "sr-RS",
+		"cz": "cs-CZ",
+		"de": "de-DE",
+		"nl": "nl-NL",
+		"it": "it-IT",
+		"fr": "fr-FR",
+		"ca": "fr-CA",
+		"es": "es-ES",
+		"pt": "pt-PT",
+		"ro": "ro-RO",
+		"tr": "tr-TR",
 
-		"fi":  "fi-FI",
-		"isl": "is-IS",
-		"nor": "nb-NO",
-		"sv":  "sv-SE",
+		"fi": "fi-FI",
+		"is": "is-IS",
+		"no": "nb-NO",
+		"sv": "sv-SE",
+		"dk": "da-DK",
 
 		"ar": "ar-SA",
 
-		"hin": "hi-IN",
+		"hi": "hi-IN",
+
+		"af": "af-ZA",
 	}
 }
